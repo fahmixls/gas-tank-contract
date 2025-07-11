239b5bc73df3d6794c9ebef2c2b5f8dfc5e8ed62
Fahmi <lukistriya.dev@gmail.com>
2025-07-11 11:49:27 +0700

    fix remapping chainlink library

--- src/Payment.sol
+++ src/Payment.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.29;
 
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
-import {ReentrancyGuard} from "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import {ReentrancyGuard} from "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
 import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
@@ -26,4 +26,6 @@ contract Payment is ReentrancyGuard, Ownable {
         IERC20 token;
         AggregatorV3Interface priceFeed;
     }
+
+    constructor() Ownable(msg.sender) {}
 }
